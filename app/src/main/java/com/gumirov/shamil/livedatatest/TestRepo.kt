7fdf104c924451cff3548950f2a23ee1e00ed3d3
Shamil Gumirov <shamil@gumirov.com>
2019-04-24 20:28:50 +0200

    Fixed setting LiveData::value background thread with postValue()

--- app/src/main/java/com/gumirov/shamil/livedatatest/TestRepo.kt
+++ app/src/main/java/com/gumirov/shamil/livedatatest/TestRepo.kt
@@ -8,7 +8,7 @@ class TestRepo {
         val res = MutableLiveData<TestData>()
         Thread(Runnable {
             Thread.sleep(1000)
-            res.value = TestData("Name", "email@b.com")
+            res.postValue(TestData("Name", "email@b.com"))
         }).start()
         return res
     }
