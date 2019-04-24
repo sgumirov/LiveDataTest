d1bcdff1a8374be1e1b3142f6646d60c3baa955e
Shamil Gumirov <shamil@gumirov.com>
2019-04-24 20:30:54 +0200

    Format TestViewModel

--- app/src/main/java/com/gumirov/shamil/livedatatest/TestViewModel.kt
+++ app/src/main/java/com/gumirov/shamil/livedatatest/TestViewModel.kt
@@ -5,12 +5,16 @@ import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class TestViewModel: ViewModel() {
-    val repo = TestRepo()
+    private val repo = TestRepo()
+
     val id = MutableLiveData<String> ()
+
     val data = Transformations.switchMap(id) { repo.load(it) }
+
     val name = Transformations.map(data) { it.name }
     val email = Transformations.map(data) { it.email }
+
     fun startLoad() {
-        id.value = "123"
+        id.value = "1"
     }
 }
